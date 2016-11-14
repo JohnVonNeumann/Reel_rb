fcadba34cc79b1158e1cf8d1aae63e3d221369c7
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-14 16:44:07 +1100

    Added welcome action to application controller and reorganised routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def welcome
+    render html: "Welcome to Reel! The best location to find the perfect fishing trip!"
+  end
+  
 end
