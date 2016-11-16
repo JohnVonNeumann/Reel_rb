459fbb43eb5509b8970422dbd686a44b6c7030e9
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-16 13:59:02 +1100

    added user name and email validation with uniqueness, upcase and lowcase, restricted length boundaries and a variety of tests in order to get the automated testing under control

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
-
+#tests are here as example pieces of code, they arent necessarily designed to
+#make sense, merely to test whether in fact our actual model code/contr code
+#is operating as it should do
 class UserTest < ActiveSupport::TestCase
 
   def setup
@@ -18,6 +20,53 @@ class UserTest < ActiveSupport::TestCase
   test "email should be present" do
     @user.email = ""
     assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_USER-ER@foo.bar.org
+                          first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+    # The method here is to make a user with the same email address as
+    # @user using @user.dup, which creates a duplicate user with the same
+    # attributes. Since we then save @user, the duplicate user has an email
+    # address that already exists in the database, and hence should not be valid.
+    # refer to regexsimpleguide.md for more ways to understand
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
 end
