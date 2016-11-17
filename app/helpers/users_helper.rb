a85a66f5af4f3cefd837c6116ba2e9b2240c81b3
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-17 11:34:24 +1100

    added gravatar functionality and made small update to user.first data in order to confirm function was operating as intended, added grav functions to show.html.erb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+  #helper files provide something of a model file for views, in this instance
+  #we are creating a file that helps the user view pull
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # In Ruby, the MD5 hashing algorithm is implemented using the
+    # hexdigest method, which is part of the Digest library:
+    # We are required to downcase the email as it will ensure our
+    # md5 hash result is downcase
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+
+    # MD5 Example
+    # >> email = "MHARTL@example.COM"
+    # >> Digest::MD5::hexdigest(email.downcase)
+    # => "1fda4469bcbec3badf5418269ffc5968"
+  end
 end
