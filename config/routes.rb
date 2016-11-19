d77dde3d8f4685cd80f77476257efccde65664cb
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-19 13:05:02 +1100

    edited rails routes and edited sessionscontrollertest get path, added documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
 
   get 'sessions/new'
-
   root 'static_pages#home'
-
   get '/help', to: 'static_pages#help'
-
   get '/contact', to: 'static_pages#contact'
-
   get '/about', to: 'static_pages#about'
-
   get '/signup', to: 'users#new'
-
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
+  # Unlike the Users resource, which used the special resources method to
+  # obtain a full suite of RESTful routes automatically (Listing 7.3),
+  # the Sessions resource will use only named routes, handling GET and POST 
+  # requests with the login route and DELETE request with the logout route.
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sessions_new_url
+    get login_path
+    # edited existing sessions_new_url path to make way for login contr changes
     assert_response :success
   end
 
