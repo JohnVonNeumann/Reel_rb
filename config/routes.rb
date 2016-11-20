71d5f50b2c37894a85963caead47c268d16c1e80
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-20 13:46:30 +1100

    updated documentation

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,16 @@ Rails.application.routes.draw do
   # the Sessions resource will use only named routes, handling GET and POST
   # requests with the login route and DELETE request with the logout route.
 
+  # our use of the resources macro route facility pertains to WHAT we are
+  # looking to do in our program, for example you can see that underneath
+  # this comment block we have resources :users, but above us we have indiv
+  # called on pathways for specific actions and their associated controllers
+  # this is because in the case of say, /contact, we dont need to be able to
+  # perform EVERY restful argument on it, only one or two, however, in the case
+  # of :users, and soon to be :listings, we need to be able to perform a range
+  # of operations on the actions, not just one or two
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
