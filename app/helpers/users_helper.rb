3328d21518d321495a0205897f468e877ff537b6
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-21 17:16:41 +1100

    commit 11 on branch, user index working as needed, mapped out inside _header and working as intended

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,13 +3,13 @@ module UsersHelper
   #we are creating a file that helps the user view pull
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, size: 80 )
-
+  def gravatar_for(user, options = { size: 80 } )
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     # In Ruby, the MD5 hashing algorithm is implemented using the
     # hexdigest method, which is part of the Digest library:
     # We are required to downcase the email as it will ensure our
     # md5 hash result is downcase
+    size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
 
