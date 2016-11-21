b657d2e4d779c7abde6cd2bd00e94d44a85d46b2
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-21 17:03:39 +1100

    Commit 9, potential game breaker, had to add in a shittonne of functions in order to get friendly forwarding set up, should probably have skipped the feature but I felt that getting in it would be easier, if there is an error anywhere, it's probably with this one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,7 @@ class UsersController < ApplicationController
     #confirms a user is logged in, disables certain actions should they not
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
