c45f6ebd13cae237b8008759361602bcd5b784d8
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-22 12:01:58 +1100

    added listings route to _header

--- db/migrate/20161121235547_create_listings.rb
+++ db/migrate/20161121235547_create_listings.rb
@@ -2,6 +2,8 @@ class CreateListings < ActiveRecord::Migration[5.0]
   def change
     create_table :listings do |t|
       t.string :title
+      t.string :listing_provider
+      t.integer :provider_id
       t.text :description
       t.string :image_url
       t.decimal :price
