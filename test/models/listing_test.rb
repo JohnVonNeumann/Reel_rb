80ed58c61289996b84638395c361b0752c29b00f
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-27 22:25:24 +1100

    added some listing model tests for valid attribute inputs and the like

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "listing attributes must not be empty" do
+    listing = Listing.new
+    assert listing.invalid?
+    assert listing.errors[:title].any?
+    assert listing.errors[:description].any?
+    assert listing.errors[:image_url].any?
+    assert listing.errors[:price].any?
+    assert listing.errors[:location].any?
+  end
+
+  test "listing price must be positive" do
+    listing  = Listing.new(title: "My Listing Title",
+                           description: "gggggg",
+                           image_url: "ggg.jpg",
+                           location: "ggg")
+    listing.price = -1
+    assert listing.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      listing.errors[:price]
+
+    listing.price = 0
+    assert listing.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      listing.errors[:price]
+
+    listing.price = 1
+    assert listing.valid?
+  end
+
+  def new_listing(image_url)
+    Listing.new(title: "My Listing Title",
+                           description: "gggggg",
+                           image_url: image_url,
+                           price: 1,
+                           location: "ggg")
+  end
+
+  test "iamge url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_listing(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_listing(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+  
 end
